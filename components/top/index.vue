3e0ee81a831f77a03c94fb30ce95df39f8961868
takashi <takashioicsn@gmail.com>
2018-05-31 11:53:44 +0900

    add toppage
    add header and footer

---
+++ components/top/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  main.l-main
+    h1 Idol Is God
+    p 僕の推しを紹介するサイト
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/top/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  <top-page/>
+</template>
+
+<script>
+  import TopPage from '~/components/Top/index.vue'
+
+  export default {
+    components: {
+      TopPage
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
